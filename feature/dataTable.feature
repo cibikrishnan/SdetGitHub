a93f6b0706c820eed45cc1eb3da86326c6721057
cibi krishnan <cibikrishnan@gmail.com>
2021-12-11 21:37:09 +0530

    Data Table examples

--- feature/dataTable.feature
+++ feature/dataTable.feature
@@ -1,5 +1,20 @@
 Feature: Data table demo
+
   @demo
   Scenario: data table for login
     Given user enters the credentials
-    |username01|password01|
+      | username  | password |
+      | ckrishnan | pwd      |
+      | ram       | rampwd   |
+
+  @demo
+  Scenario: data for fields
+    When I enter valid data on the page
+      | Fields                 | Values              |
+      | First Name             | Tom                 |
+      | Last Name              | Kenny               |
+      | Email Address          | someone@someone.com |
+      | Re-enter Email Address | someone@someone.com |
+      | Password               | Password1           |
+      | Birthdate              | 01                  |
+
--- src/test/java/stepdefinitions/MyStepdefs.java
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -10,18 +10,30 @@ import java.util.Map;
 
 public class MyStepdefs {
 
-        @Given("^user enters the credentials$")
-        public void user_enters_the_credentials(DataTable userCredentials) {
+    @Given("^user enters the credentials$")
+    public void user_enters_the_credentials(DataTable userCredentials) {
+        List<Map<String, String>> data = userCredentials.asMaps(String.class, String.class);
 
-            List<String> data = userCredentials.row(0);
-            Map<String, String> credentials = new HashMap<>();
-            credentials.put("username", data.get(0));
-            credentials.put("password", data.get(1));
+        for (int i = 0; i < data.size(); i++) {
+            int j = i + 1;
+            System.out.println("Username in the row: " + j + " : " + data.get(i).get("username"));
+            System.out.println("password in the row: " + j + " : " + data.get(i).get("password"));
 
-            System.out.println("Username is: " + credentials.get("username"));
-            System.out.println("Password is: " + credentials.get("password"));
+        }
+
+    }
+
+    @Given("^I enter valid data on the page$")
+    public void enterData(DataTable table) {
+        List<Map<String, String>> fieldValue = table.asMaps(String.class, String.class);
+
+        for (int i = 0; i < fieldValue.size(); i++) {
+
+            System.out.println("field " +  fieldValue.get(i).get("Fields") + " has the value " + fieldValue.get(i).get("Values"));
 
 
         }
+
+    }
 
 }
