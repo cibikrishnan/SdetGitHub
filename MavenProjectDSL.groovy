4423c4a9f285e75604e45d5d8fb1f29078548f25
cibi krishnan <cibikrishnan@gmail.com>
2022-01-26 15:38:00 +0530

    Updated sure-fire plugin Added MavenProjectDSL.groovy Updated MavenProjectDSL.groovy file

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,17 +1,20 @@
 job('Job Name') {
-    description("Maven DSL Demo on ${new Date()}")
+    description('Maven DSL Demo on ${new Date()}')
     scm {
 
-        git("https://github.com/cibikrishnan/SdetGitHub.git", 'master')
+        git('https://github.com/cibikrishnan/SdetGitHub.git', 'master')
     }
-            {
-                triggers{
-                    scm('*****')
-                }
-            }
-    steps{
 
-        Maven('clean install test', 'pom.xml')
+    triggers {
+        scm('*****')
+    }
+
+    steps {
+
+        maven('clean install', 'pom.xml') {
+            goals('test')
+        }
+
 
     }
 
